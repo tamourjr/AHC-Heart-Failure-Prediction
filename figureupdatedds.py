0e80064288c04144f25bf3f4b2726982c5872dab
tamourjr <ayatamer389@gmail.com>
2025-05-21 18:33:38 +0300

    updates

--- figureupdatedds.py
+++ figureupdatedds.py
@@ -9,7 +9,11 @@ df.to_csv('heart_updated.csv', index=False)
 
 
 
-def hist_sex_disease(df):
+def hist_sex_disease():
+    import pandas as pd
+    import plotly.express as px
+
+    df = pd.read_csv('heart.csv')
     fig = px.histogram(df, x='Sex', color='HeartDisease', barmode='group',
                        title='Heart Disease Distribution by Gender', text_auto=True)
     return fig
